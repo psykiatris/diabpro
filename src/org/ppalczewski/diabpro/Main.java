0440d130e83c15a7123e4ee60c3ddd62124c430a
Patrick Palczewski <psykiatris@gmail.com>
2018-06-15 08:21:19 -0700

    Added form, updated Main.java

--- src/org/ppalczewski/diabpro/Main.java
+++ src/org/ppalczewski/diabpro/Main.java
@@ -1,5 +1,7 @@
+package org.ppalczewski.diabpro;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This will run Diabetes Pro.");
+        System.out.println("Runs Diabetes Pro.");
     }
 }
---
+++ src/org/ppalczewski/diabpro/resourcxe/Diary.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2018. Author: Patrick Palczewski <psykiatris@gmail.com>. Licensed under GPL 3. See LICENSE for details.
+ */
+
+package org.ppalczewski.diabpro.resourcxe;
+
+import javax.swing.*;
+
+public class Diary {
+    private JLabel diaryLabel;
+    private JTextPane textPane1;
+}
