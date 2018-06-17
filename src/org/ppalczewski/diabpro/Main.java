c82580430458f7c893a456636c38a90ed70eff7e
Patrick Palczewski <psykiatris@gmail.com>
2018-06-17 08:58:27 -0700

    A little restructure into separate packages.

--- src/org/ppalczewski/diabpro/Main.java
+++
@@ -1,7 +0,0 @@
-package org.ppalczewski.diabpro;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Runs Diabetes Pro.");
-    }
-}
---
+++ src/org/ppalczewski/diabpro/core/Main.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2018. Author: Patrick Palczewski <psykiatris@gmail.com>. Licensed under GPL 3. See LICENSE for details.
+ */
+
+package org.ppalczewski.diabpro.core;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Runs Diabetes Pro.");
+    }
+}
