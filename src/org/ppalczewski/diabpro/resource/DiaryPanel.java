e7a5815fc02231d2b58211c48b40447f2728052b
Patrick Palczewski <psykiatris@gmail.com>
2018-06-25 11:36:41 -0700

    Created several panels for the diary form.

---
+++ src/org/ppalczewski/diabpro/resource/DiaryPanel.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2018. Author: Patrick Palczewski <psykiatris@gmail.com>. Licensed under GPL 3. See LICENSE for details.
+ */
+
+package org.ppalczewski.diabpro.resource;
+
+import javax.swing.*;
+
+public class DiaryPanel {
+    private JEditorPane editorDiary;
+    private JPanel diaryPanel;
+}
---
+++ src/org/ppalczewski/diabpro/resource/datePanel.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2018. Author: Patrick Palczewski <psykiatris@gmail.com>. Licensed under GPL 3. See LICENSE for details.
+ */
+
+package org.ppalczewski.diabpro.resource;
+
+import javax.swing.*;
+
+public class datePanel {
+    private JPanel pnlDate;
+}
