60a1624d0c3f403e569b24970a042dda02dd767f
Patrick Palczewski <psykiatris@gmail.com>
2018-08-17 10:51:39 -0700

    Created database connection code/ Added mysql jar file to project.

---
+++ src/org/ppalczewski/diabpro/core/ConnectDatabase.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2018. Author: Patrick Palczewski <psykiatris@gmail.com>. Licensed under GPL 3. See LICENSE for details.
+ */
+
+package org.ppalczewski.diabpro.core;
+
+/*
+This file will connect the diary to the databse. Going to use mySQL.
+
+The date will be the key for each entry.
+Fields will be:
+* Glucose readings
+* Nutrition stats
+* Diary notes
+ */
+// Imports
+import com.sun.xml.internal.bind.v2.TODO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDatabase {
+
+    // Set up connection
+    // TODO: 8/17/18 Set up way to get user info
+
+    // using established test database for now
+    // Change to the diabetespro database when done
+    private static final String url = "jdbc:mysql://localhost:3306/test" +
+            "?verifyServerCertificate=false&useSSL=true";
+    private static final String user = "root";
+    private static final String pw = "Pe99er@1";
+    public static Connection con;
+
+    public static void doConnect() {
+        if(con != null) return;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, pw);
+            System.out.println("Connected");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Make sure database connection closed
+    public static void close() {
+        if(con != null) {
+            try {
+                con.close();
+                System.out.println("Closed connection");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    } // End close()
+
+    public static void createDatabase() {
+        // ...
+    }
+
+    public static void editDatabase() {
+        // ...
+    }
+
+
+} // End class
+
--- src/org/ppalczewski/diabpro/core/Main.java
+++ src/org/ppalczewski/diabpro/core/Main.java
@@ -5,8 +5,12 @@
 package org.ppalczewski.diabpro.core;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Runs Diabetes Pro.");
+        System.out.println("Connecting to database");
+        ConnectDatabase.doConnect();
+        ConnectDatabase.close();
 
     }
 }
