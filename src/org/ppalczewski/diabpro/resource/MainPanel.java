601fc90cd41bf66d6d2feb7dc825999b4aa74e98
Patrick Palczewski <psykiatris@gmail.com>
2018-08-17 17:16:08 -0700

    Updated forms, but probably need to do it again, as they must be bound to a class

---
+++ src/org/ppalczewski/diabpro/resource/MainPanel.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018. Author: Patrick Palczewski <psykiatris@gmail.com>. Licensed under GPL 3. See LICENSE for details.
+ */
+
+package org.ppalczewski.diabpro.resource;
+
+import com.michaelbaranov.microba.calendar.DatePicker;
+
+import javax.swing.*;
+
+public class MainPanel {
+    private JPanel main;
+    private JEditorPane editorPane1;
+    private DatePicker datepick;
+    private JList statslist;
+}
