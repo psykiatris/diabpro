cf01ad045b8bd73e193952b4dfcb047f08d75166
Patrick Palczewski <psykiatris@gmail.com>
2018-10-03 19:02:17 -0700

    Added a class to manage file functions, saving, loading, etc.

---
+++ src/org/ppalczewski/diabpro/core/DiaryLoadSave.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018. Author: Patrick Palczewski <psykiatris@gmail.com>. Licensed under GPL 3. See LICENSE for details. 
+ */
+
+package org.ppalczewski.diabpro.core;
+/*
+Mechanism to load/save diary info.
+ */
+public class DiaryLoadSave {
+    
+    void loadDiary{} {
+        // TODO: 10/3/18 Set up mechanism to load diary in the GUI. 
+    }
+    
+    void saveDiary() {
+        // TODO: 10/3/18 Set up mechanism to sve diary from GUI. 
+    }
+}
